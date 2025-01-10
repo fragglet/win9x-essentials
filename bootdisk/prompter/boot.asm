6f0763f7671b53338c3e58ae88291e1330ee01a0
Simon Howard <fraggle@soulsphere.org>
2025-01-10 14:23:19 -0500

    Print progress dots and detect enter keypress

--- bootdisk/prompter/boot.asm
+++ bootdisk/prompter/boot.asm
@@ -1,5 +1,7 @@
 %define BIOS_READ_KEY        0x00
+%define BIOS_CHECK_KEY       0x01
 %define BIOS_PRINT_CHAR      0x0e
+%define BIOS_GET_SYSTEM_TIME 0x00
 
 org 0x7c00
 
@@ -14,20 +16,38 @@ real_start:
     push cs              ; ds=cs
     pop ds
 
-    mov al, '!'
+    mov al, '?'
     call print_char
 
 loop:
-    call read_key
-    test al, al
-    jz loop
+    call check_enter_pressed
+    jz enter_pressed
+
+    mov al, '.'
     call print_char
+
+    call sleep_one_tick
     jmp loop
 
-; Returns ah=scancode, al=ascii
-read_key:
+enter_pressed:
+    mov al, '!'
+    call print_char
+    jmp forever
+
+; Returns zf unset if enter has been pressed
+check_enter_pressed:
+    mov ah, BIOS_CHECK_KEY
+    int 0x16
+    jz enter_not_pressed
+
     mov ah, BIOS_READ_KEY
     int 0x16
+    cmp al, 13
+    ret
+
+enter_not_pressed:
+    mov ah, 0
+    cmp ah, 1
     ret
 
 ; Call with al=key
@@ -36,6 +56,30 @@ print_char:
     mov bx, 0x000f
     int 0x10
     ret
+
+; Sleeps for 1/18.2 of a second and then returns
+sleep_one_tick:
+    mov ah, BIOS_GET_SYSTEM_TIME
+    mov al, 0x00
+    int 0x1a
+    push cx
+    push dx
+
+sleep_loop:
+    mov ah, BIOS_GET_SYSTEM_TIME
+    mov al, 0x00
+    int 0x1a
+    pop bx
+    pop ax
+
+    cmp ax, cx              ; time advanced?
+    jne do_ret
+    cmp bx, dx
+    jne do_ret
+
+    push ax
+    push bx
+    jmp sleep_loop
 
 forever:
     jmp forever
